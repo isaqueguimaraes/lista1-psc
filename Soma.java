2aac7cf42ccdbe99d74f041b4d41454ccde716a4
Isaque <isaquegabriel2112206@gmail.com>
2025-04-11 16:25:31 -0300

    Add files via upload

---
+++ Soma.java
@@ -0,0 +1,12 @@
+public class Soma {
+  public static void main(String[] args) {
+
+    int soma = 0;
+
+    for (int i = 1; i <= 15; i++) {
+      soma += i;
+    }
+
+    System.out.println("A soma dos números de 1 a 15 é: " + soma);
+  }
+}
